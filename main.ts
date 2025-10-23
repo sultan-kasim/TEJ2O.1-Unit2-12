e7109172360a694cd8acd1b14c278f362afe3d83
sultan-kasim <sultan.kasim@stu.ocsb.ca>
2025-10-23 13:27:05 -0400

    This program turns on red if to close and green not too close.

--- main.ts
+++ main.ts
@@ -1,8 +1,43 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Sultan
+ * Created on: Oct 2025
+ * This program turns on red if to close aand green not too close.
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceToObject: number = 0
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+// setup
+basic.showIcon(IconNames.Happy)
+
+// find distance from sonar
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showNumber(distanceToObject)
+
+    // check distance and light up neoPixel
+    if (distanceToObject <= 10) {
+
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+    } else {
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+        strip.show()
+    }
+
+    basic.showIcon(IconNames.Happy)
+})
