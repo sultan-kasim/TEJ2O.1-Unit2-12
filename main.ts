1a5eb917ab4df6f746b820b1cd00158a28814801
sultan-kasim <sultan.kasim@stu.ocsb.ca>
2025-10-24 13:02:38 -0400

    urns on red if to close aand green not too close.

--- main.ts
+++ main.ts
@@ -4,31 +4,46 @@
  * Created on: Oct 2025
  * This program turns on red if to close aand green not too close.
 */
+
+
 // variables
-let distanceToObject: number = 0
-let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+let distanceToObject: number = 0;
 
-//setup
-basic.showIcon(IconNames.Happy)
+let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB);
 
-// find distance from sonar
+// setup
+basic.showIcon(IconNames.Happy);
+
+// find distance from sonar and trigger the alert
 input.onButtonPressed(Button.A, function () {
-    basic.clearScreen()
+
+    basic.clearScreen();
+
+    // measure distance using the sonar sensor (P1=Trigger, P2=Echo)
     distanceToObject = sonar.ping(
         DigitalPin.P1,
         DigitalPin.P2,
         PingUnit.Centimeters
-    )
-    basic.showNumber(distanceToObject)
+    );
 
-    // shows red if too close and shows green if it's not
+    basic.showNumber(distanceToObject);
+
+    // shows Red if too close and shows Green if it's not (flashes for 1 second)
+
     if (distanceToObject >= 10) {
-        strip.showColor(neopixel.colors(NeoPixelColors.Green))
-        basic.pause(1000)
-        strip.showColor(neopixel.colors(NeoPixelColors.Black))
+    
+        strip.showColor(neopixel.colors(NeoPixelColors.Green));
+        basic.pause(1000);
+        
+        strip.showColor(neopixel.colors(NeoPixelColors.Black));
     } else {
-        strip.showColor(neopixel.colors(NeoPixelColors.Red))
-        basic.pause(1000)
-        strip.showColor(neopixel.colors(NeoPixelColors.Black))
+
+        strip.showColor(neopixel.colors(NeoPixelColors.Red));
+        basic.pause(1000);
+        
+        strip.showColor(neopixel.colors(NeoPixelColors.Black));
     }
-})
+
+  
+    basic.showIcon(IconNames.Happy);
+});
